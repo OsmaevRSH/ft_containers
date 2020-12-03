3f1e7370fa38c06bf219813ba51cf416d16a5d96
OsmaevRSH <osmaev.ruslan.bmstu@gmail.com>
2020-12-03 21:00:55 +0300

    add push_back

--- Containers/Vector.hpp
+++ Containers/Vector.hpp
@@ -11,24 +11,30 @@ namespace ft
 		private:
 			typename T *ptr;
 			size_t vector_len;
+			size_t alloc_size;
 		public:
 			explicit vector();
 			explicit vector(size_t n, const T &val = value_type());
 			template<class InputIterator>
 			vector(InputIterator first, InputIterator last);
-			vector (const vector& x);
+			vector(const vector &x);
+			~vector();
+
+			vector &operator=(const vector &x)
+			void push_back(const value_type &val);
 	};
 
 	template<class T>
-	vector<T>::vector() : vector_len(0)
+	vector<T>::vector() : vector_len(0), alloc_size(0)
 	{
 		ptr = new T[0];
 	}
 
 	template<class T>
-	vector<T>::vector(size_t n, const T &val) : vector_len(n)
+	vector<T>::vector(size_t n, const T &val)
+		: vector_len(n), alloc_size((n / 8) * 8 + 8)
 	{
-		ptr = new T[n];
+		ptr = new T[(n / 8) * 8 + 8];
 		for (int i = 0; i < n; ++i)
 			ptr[i] = val;
 	}
@@ -42,7 +48,7 @@ namespace ft
 		for (; copy_first_iter < last; ++copy_first_iter)
 			++size;
 		vector_len = size;
-		ptr = new T[size];
+		ptr = new T[(size / 8) * 8 + 8];
 		for (int i = 0; i < size; ++i)
 		{
 			ptr[i] = *first;
@@ -51,11 +57,54 @@ namespace ft
 	}
 
 	template<class T>
-	vector<T>::vector(const vector &x) : vector_len(x.vector_len)
+	vector<T>::vector(const vector &x)
+		: vector_len(x.vector_len), alloc_size(x.alloc_size)
 	{
 		ptr = new T[x.vector_len];
 		for (int i = 0; i < x.vector_len; ++i)
 			ptr[i] = x.ptr[i];
+	}
+
+	template<class T>
+	vector<T>::~vector()
+	{
+		delete[] ptr;
+	}
+
+	template<class T>
+	vector &vector<T>::operator=(const vector &x)
+	{
+		delete[] ptr;
+		vector_len = x.vector_len;
+		alloc_size = x.alloc_size;
+		ptr = new T[alloc_size];
+		for (int i = 0; i < vector_len; ++i)
+			ptr[i] = x.ptr[i];
+		return *this;
+	}
+
+	template<class T>
+	void vector<T>::push_back(const value_type &val)
+	{
+		if (vector_len < alloc_size - 1)
+		{
+			ptr[vector_len] = val;
+			++vector_len;
+		}
+		else
+		{
+			T tmp_ptr = new T[alloc_size];
+			for (int i = 0; i < alloc_size; ++i)
+				tmp_ptr[i] = ptr[i];
+			alloc_size += 8;
+			delete[] ptr;
+			ptr = new T[alloc_size];
+			for (int i = 0; i < alloc_size; ++i)
+				ptr[i] = tmp_ptr[i];
+			ptr[++vector_len] = val;
+			for (int i = vector_len; i < alloc_size; ++i)
+				ptr[i] = 0;
+		}
 	}
 }
 #endif
