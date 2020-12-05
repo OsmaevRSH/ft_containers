dd0debcc275e4de64eeedec2c992d72be886e562
ltheresi <osmaev.ruslan.bmstu@gmail.com>
2020-12-05 17:59:11 +0300

    add simple tests

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -2,6 +2,7 @@
 #define FT_CONTAINERS_VECTOR_HPP
 
 #include <iostream>
+#include <cmath>
 #include "../iterator/random_access_iterator.hpp"
 
 namespace ft
@@ -11,14 +12,14 @@ namespace ft
 	{
 		private:
 			T *ptr;
-			size_t vector_len;
-			size_t alloc_size;
+			int vector_len;
+			int alloc_size;
 
 		public:
 
 			//Constructor
 			explicit vector();
-			explicit vector(size_t n, const T &val);
+			explicit vector(size_t n, const T &val = T());
 			template<class InputIterator>
 			vector(InputIterator first, InputIterator last);
 			vector(const vector &x);
@@ -35,6 +36,14 @@ namespace ft
 			friend bool operator<=(const vector<T> &lhs, const vector<T> &rhs);
 			friend bool operator>(const vector<T> &lhs, const vector<T> &rhs);
 			friend bool operator>=(const vector<T> &lhs, const vector<T> &rhs);
+
+			//Capacity
+			size_t size() const;
+			size_t max_size() const;
+			size_t capacity() const;
+			void resize(size_t n, T val = T());
+			void reserve(size_t n);
+			bool empty() const;
 
 			//Function
 			void push_back(const T &val);
@@ -61,7 +70,7 @@ namespace ft
 
 	template<class T>
 	vector<T>::iterator::iterator(const vector::iterator &copy)
-		: Random_access_iterator<T>(copy)
+			: Random_access_iterator<T>(copy)
 	{
 	}
 
@@ -91,9 +100,9 @@ namespace ft
 
 	template<class T>
 	vector<T>::vector(size_t n, const T &val)
-		: vector_len(n), alloc_size((n / 8) * 8 + 8)
+			: vector_len(n), alloc_size(ceil(log(n)/log(2)))
 	{
-		ptr = new T[(n / 8) * 8 + 8];
+		ptr = new T[(int)pow(2, ceil(log(vector_len)/log(2)))];
 		for (int i = 0; i < n; ++i)
 			ptr[i] = val;
 	}
@@ -107,8 +116,8 @@ namespace ft
 		for (; copy_first_iter < last; ++copy_first_iter)
 			++size;
 		vector_len = size;
-		ptr = new T[(size / 8) * 8 + 8];
-		alloc_size = (size / 8) * 8 + 8;
+		ptr = new T[(int)pow(2, ceil(log(vector_len)/log(2)))];
+		alloc_size = (int)pow(2, ceil(log(vector_len)/log(2)));
 		for (int i = 0; i < size; ++i)
 		{
 			ptr[i] = *first;
@@ -118,7 +127,7 @@ namespace ft
 
 	template<class T>
 	vector<T>::vector(const vector &x)
-		: vector_len(x.vector_len), alloc_size(x.alloc_size)
+			: vector_len(x.vector_len), alloc_size(x.alloc_size)
 	{
 		ptr = new T[x.vector_len];
 		for (int i = 0; i < x.vector_len; ++i)
@@ -156,17 +165,13 @@ namespace ft
 		}
 		else
 		{
-			T tmp_ptr = new T[alloc_size];
-			for (int i = 0; i < alloc_size; ++i)
+			++vector_len;
+			T *tmp_ptr = new T[alloc_size = pow(2, ceil(log(vector_len)/log(2)))];
+			for (int i = 0; i < vector_len - 1; ++i)
 				tmp_ptr[i] = ptr[i];
-			alloc_size += 8;
+			tmp_ptr[vector_len - 1] = val;
 			delete[] ptr;
-			ptr = new T[alloc_size];
-			for (int i = 0; i < alloc_size; ++i)
-				ptr[i] = tmp_ptr[i];
-			ptr[++vector_len] = val;
-			for (int i = vector_len; i < alloc_size; ++i)
-				ptr[i] = 0;
+			ptr = tmp_ptr;
 		}
 	}
 	template<class T>
@@ -220,6 +225,71 @@ namespace ft
 	typename vector<T>::iterator &vector<T>::end()
 	{
 		return iterator(this->ptr[this->vector_len]);
+	}
+
+	template<class T>
+	size_t vector<T>::size() const
+	{
+		return vector_len;
+	}
+
+	template<class T>
+	size_t vector<T>::capacity() const
+	{
+		return alloc_size;
+	}
+
+	template<class T>
+	bool vector<T>::empty() const
+	{
+		return vector_len == 0;
+	}
+
+	template<class T>
+	size_t vector<T>::max_size() const
+	{
+		size_t tmp = -1 / sizeof(T);
+		return tmp;
+	}
+
+	template<class T>
+	void vector<T>::resize(size_t n, T val)
+	{
+		if (n < vector_len)
+		{
+			T *tmp = new T[n];
+			for (int i = 0; i < n; ++i)
+				tmp[i] = ptr[i];
+			delete[] ptr;
+			ptr = tmp;
+		}
+		else
+		{
+			T *tmp = new T[n];
+			for (int i = 0; i < vector_len; ++i)
+				tmp[i] = ptr[i];
+			for (int i = vector_len; i < n; ++i)
+				tmp[i] = val;
+			delete[] ptr;
+			ptr = tmp;
+		}
+		if (n > alloc_size)
+			alloc_size = pow(2, ceil(log(n)/log(2)));
+		vector_len = n;
+	}
+
+	template<class T>
+	void vector<T>::reserve(size_t n)
+	{
+		if (n > alloc_size)
+		{
+			T *tmp = new T[n];
+			for (int i = 0; i < vector_len; ++i)
+				tmp[i] = ptr[i];
+			delete[] ptr;
+			ptr = tmp;
+			alloc_size = n;
+		}
 	}
 }
 
--- vector/vector_main.cpp
+++ vector/vector_main.cpp
@@ -1,24 +1,131 @@
 #include <iostream>
 #include <vector>
 #include "vector.hpp"
+#include "../test.hpp"
+#include <math.h>
+#include <sstream>
 
 using namespace std;
 
+template<class T>
+void capacity_part(const vector<T> &first, const ft::vector<T> &second)
+{
+	cout << BLUE << "vector.size(): " << RESET;
+	if (first.size() == second.size())
+		cout << GREEN << "[" << "OK" << "]" << RESET << endl;
+	else
+	{
+		cout << RED << "[" << "FAILED" << "]" << endl;
+		cout << "vector: " << first.size() << endl;
+		cout << "ft::vector: " << second.size() << RESET << endl;
+	}
+	cout << BLUE << "vector.capacity(): " << RESET;
+	if (first.capacity() == second.capacity())
+		cout << GREEN << "[" << "OK" << "]" << RESET << endl;
+	else
+	{
+		cout << RED << "[" << "FAILED" << "]" << endl;
+		cout << "vector: " << first.capacity() << endl;
+		cout << "ft::vector: " << second.capacity() << RESET << endl;
+	}
+	cout << BLUE << "vector.empty(): " << RESET;
+	if (first.empty() == second.empty())
+		cout << GREEN << "[" << "OK" << "]" << RESET << endl;
+	else
+	{
+		cout << RED << "[" << "FAILED" << "]" << endl;
+		cout << "vector: " << first.empty() << endl;
+		cout << "ft::vector: " << second.empty() << RESET << endl;
+	}
+	cout << BLUE << "vector.max_size(): " << RESET;
+	if (first.max_size() == second.max_size())
+		cout << GREEN << "[" << "OK" << "]" << RESET << endl;
+	else
+	{
+		cout << RED << "[" << "FAILED" << "]" << endl;
+		cout << "vector: " << first.max_size() << endl;
+		cout << "ft::vector: " << second.max_size() << RESET << endl;
+	}
+}
+
+template<class T>
+void vec_resize(vector<T> &first, ft::vector<T> &second, int n, T value)
+{
+	cout << BLUE << "---------------" << endl << RESET;
+	first.resize(n, value);
+	second.resize(n, value);
+	stringstream one;
+	stringstream two;
+	cout << BLUE << "vector.resize(" << n << ", " << value << "): " << RESET;
+	for (int i = 0; i < first.size(); ++i)
+		one << first[i] << " ";
+	for (int i = 0; i < second.size(); ++i)
+		two << second[i] << " ";
+	if (one.str() == two.str())
+		cout << GREEN << "[" << "OK" << "]" << RESET << endl;
+	else
+	{
+		cout << RED << "[" << "FAILED" << "]" << endl;
+		cout << "vector: " << one.str() << endl;
+		cout << "ft::vector: " << two.str() << endl << RESET;
+	}
+	capacity_part(first, second);
+}
+
+template<class T>
+void vec_reserve(vector<T> &first, ft::vector<T> &second, int n)
+{
+	cout << BLUE << "---------------" << endl << RESET;
+	cout << BLUE << "vector.reserve(): " << RESET;
+	first.reserve(n);
+	second.reserve(n);
+	if (first.capacity() == second.capacity())
+		cout << GREEN << "[" << "OK" << "]" << RESET << endl;
+	else
+	{
+		cout << "[" << "FAILED" << "]" << endl;
+		cout << "vector: " << first.capacity() << endl;
+		cout << "ft::vector: " << second.capacity() << endl;
+	}
+	capacity_part(first, second);
+}
+
+template<class T>
+void vec_push_back(vector<T> &first, ft::vector<T> &second)
+{
+	cout << BLUE << "---------------" << endl << RESET;
+	cout << BLUE << "vector.push_back(): " << RESET;
+	stringstream one;
+	stringstream two;
+	for (int i = 0; i < 100; ++i)
+	{
+		first.push_back(pow(i, 2));
+		second.push_back(pow(i, 2));
+	}
+	for (int i = 0; i < first.size(); ++i)
+		one << first[i] << " ";
+	for (int i = 0; i < second.size(); ++i)
+		two << second[i] << " ";
+	if (one.str() == two.str())
+		cout << GREEN << "[" << "OK" << "]" << RESET << endl;
+	else
+	{
+		cout << RED << "[" << "FAILED" << "]" << endl;
+		cout << "vector: " << one.str() << endl;
+		cout << "ft::vector: " << two.str() << endl << RESET;
+	}
+	capacity_part(first, second);
+}
+
 int main()
 {
-	cout << "=====================vector()=====================\n";
+	cout << YELLOW << "=====================vector()=====================\n" << RESET;
 	vector<int> first;
-	first.push_back(1);
-	first.push_back(1);
-	first.push_back(1);
-	first.push_back(1);
-	first.push_back(1);
-	first.push_back(1);
-	first.push_back(1);
-	first.push_back(1);
-	first.push_back(1);
-	cout << first.size() << endl;
-	cout << first.capacity() << endl;
-	cout << first.empty() << endl;
-	cout << first.max_size() << endl;
+	ft::vector<int> second;
+	capacity_part(first, second);
+	vec_resize(first, second, 4, 2);
+	vec_resize(first, second, 7, 10);
+	vec_resize(first, second, 2, int());
+	vec_reserve(first, second, 1000);
+	vec_push_back(first, second);
 }
