95122a3dbdcf5138ae1b2672ecbe5a484c8239c1
Ruslan Osmaev <osmaev.ruslan.bmstu@gmail.com>
2021-06-17 03:34:22 +0300

    queue done

---
+++ Container/queue.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace ft
+{
+	template<class T, class Container = list <T> >
+	class queue
+	{
+		public:
+			typedef T value_type;
+			typedef Container container_type;
+			typedef size_t size_type;
+
+		private:
+			Container base;
+
+		public:
+			explicit queue(const container_type &container = container_type()) : base(container) {}
+			bool empty() const { return base.empty(); }
+			size_type size() const { return base.size(); }
+			value_type &front() { return base.front(); }
+			const value_type &front() const { return base.front(); }
+			value_type &back() { return base.back(); }
+			const value_type &back() const { return base.back(); }
+			void push(const value_type &val) { base.push_back(val); }
+			void pop() { base.pop_back(); }
+
+			friend bool operator==(const queue<T, Container> &lhs, const queue<T, Container> &rhs) { lhs.base == rhs.base; }
+			friend bool operator!=(const queue<T, Container> &lhs, const queue<T, Container> &rhs) { lhs.base != rhs.base; }
+			friend bool operator<(const queue<T, Container> &lhs, const queue<T, Container> &rhs) { lhs.base < rhs.base; }
+			friend bool operator>(const queue<T, Container> &lhs, const queue<T, Container> &rhs) { lhs.base > rhs.base; }
+			friend bool operator<=(const queue<T, Container> &lhs, const queue<T, Container> &rhs) { lhs.base <= rhs.base; }
+			friend bool operator>=(const queue<T, Container> &lhs, const queue<T, Container> &rhs) { lhs.base >= rhs.base; }
+	};
+}
